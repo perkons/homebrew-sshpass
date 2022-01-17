6f78ee8f3afe5e1fc5445875092c6fdd2acf7703
perkons <42235343+perkons@users.noreply.github.com>
2022-01-17 11:36:40 +0100

    Fix Tap

--- sshpass.rb
+++ sshpass.rb
@@ -5,11 +5,15 @@ class Sshpass < Formula
   sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
   license "GPL-2.0-or-later"
 
-  def install
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    def install
+      system "./configure", "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}"
+      system "make", "install"
+    end
+  
+    test do
+      system "sshpass"
+    end
   end
-
-  test do
-    system "false"
-  end
-end
